cd89e0f4d706d9e8038c6a8838baeea249be1d9c
Dietrich Epp <depp@zdome.net>
2025-03-07 17:13:25 -0500

    Create window with winit
    When run, the program will open a small window on-screen. You can close
    the window, and the program will exit when you do.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if self.window.is_none() {
+            self.window = Some(
+                event_loop
+                    .create_window(Window::default_attributes())
+                    .unwrap(),
+            );
+        }
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            _ => (),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut app = App::default();
+    event_loop.run_app(&mut app).unwrap();
 }
