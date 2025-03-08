ab5a9ceaad9a4eafa1443fcec50a5a4dce9b52e8
Dietrich Epp <depp@zdome.net>
2025-03-08 01:03:22 -0500

    Create window with GLFW

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
+#include <GLFW/glfw3.h>
+
 #include <cstdio>
+#include <cstdlib>
+
+namespace
+{
+
+extern "C" void ErrorCallback(int error, const char *description)
+{
+	std::fprintf(stderr, "Error: %s\n", description);
+}
+
+} // namespace
 
 int main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
-	std::puts("Hello, world!");
+
+	glfwSetErrorCallback(ErrorCallback);
+	if (!glfwInit()) {
+		std::exit(1);
+	}
+
+	GLFWwindow *window =
+		glfwCreateWindow(640, 480, "Later, Darker", nullptr, nullptr);
+	if (window == nullptr) {
+		glfwTerminate();
+		exit(1);
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
