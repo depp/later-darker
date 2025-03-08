566090225dca3f75ecfe97bd05f64076f3c05f27
Dietrich Epp <depp@zdome.net>
2025-03-08 01:19:08 -0500

    Create OpenGL context
    The program now shows a window with a solid color. The color is animated
    and goes through the rainbow.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <GLFW/glfw3.h>
+#include <gl/GL.h>
 
+#include <cmath>
 #include <cstdio>
 #include <cstdlib>
+#include <numbers>
 
 namespace
 {
@@ -31,10 +34,28 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
 	while (!glfwWindowShouldClose(window)) {
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+
+		double time = glfwGetTime();
+		constexpr float d = std::numbers::pi_v<float> * 2.0f / 3.0f;
+		constexpr double rate = 0.3;
+		float a = static_cast<float>(std::fmod(time * rate, 1.0)) *
+		          (2.0f * std::numbers::pi_v<float>);
+		glClearColor(0.5f + 0.5f * std::sin(a + d), 0.5f + 0.5f * std::sin(a),
+		             0.5f + 0.5f * std::sin(a - d), 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glfwDestroyWindow(window);
 
 	glfwTerminate();
 }
