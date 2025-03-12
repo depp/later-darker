9f9540faeeaf118abfdc5cf49f8bbd73dcfe12fa
Dietrich Epp <depp@zdome.net>
2025-03-12 11:33:33 -0400

    Disable test logging

--- src/main.cpp
+++ src/main.cpp
@@ -57,10 +57,12 @@ extern "C" void ErrorCallback(int error, const char *description) {
 
 void Main() {
 	log::Init();
+	/*
 	DumpEnv();
 	LOG(Info, "Test 2-byte.", log::Attr{"str", L"Πισθέταιρος"});
 	LOG(Info, "Test 3-byte.", log::Attr{"str", L"吾輩は猫である"});
 	LOG(Info, "Test 4-byte.", log::Attr{"str", L"Grin😀"});
+	*/
 
 	glfwSetErrorCallback(ErrorCallback);
 	if (!glfwInit()) {
