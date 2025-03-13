d5d87528990909d783dc0a6633158f7a9d8dddbe
Dietrich Epp <depp@zdome.net>
2025-03-13 11:22:38 -0400

    Use OpenGL framework header on macOS

--- src/gl.hpp
+++ src/gl.hpp
@@ -5,4 +5,17 @@
 
 // This file provides the OpenGL API.
 
+#if __APPLE__
+// On macOS, an OpenGL loader is not necessary. We can just get the definitions
+// directly from the OpenGL framework.
+
+// OpenGL is deprecated on macOS. We don't care. This silences the warnings.
+#define GL_SILENCE_DEPRECATION 1
+
+#include <OpenGL/gl3.h>
+
+#else
+
 #include <glad/gl.h>
+
+#endif
