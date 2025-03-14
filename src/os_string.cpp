34a267caf43ebacf6c7c970cd25a9734f7c2a5db
Dietrich Epp <depp@zdome.net>
2025-03-14 12:30:37 -0400

    Remove unnecessary #pragma once

--- src/os_string.cpp
+++ src/os_string.cpp
@@ -1,7 +1,6 @@
 // Copyright 2025 Dietrich Epp <depp@zdome.net>
 // Licensed under the Mozilla Public License Version 2.0.
 // SPDX-License-Identifier: MPL-2.0
-#pragma once
 #include "os_string.hpp"
 
 #include "log.hpp"
