d6d53fd3fb8f6b46886b36962e8484c9e4558a3b
Dietrich Epp <depp@zdome.net>
2025-03-17 17:39:06 -0400

    Refactor dev build shader loading
    The code path differences between the hot-reload path and the embedded
    shader source path is minimized.

--- src/gl_shader_dev.cpp
+++ src/gl_shader_dev.cpp
@@ -9,10 +9,6 @@
 #include "var.hpp"
 
 #include <array>
-#include <cstdio>
-#include <cstring>
-#include <limits>
-#include <string>
 #include <string_view>
 
 namespace demo {
@@ -20,129 +16,150 @@ namespace gl_shader {
 
 namespace {
 
-// Compile a shader from the given source code.
-GLuint CompileShader(GLenum shaderType, std::string_view fileName) {
-	std::vector<unsigned char> data;
-	std::string filePath{"shader/"};
-	filePath.append(fileName);
-	if (!ReadFile(&data, filePath)) {
-		FAIL("Could not read shader.", log::Attr{"file", fileName});
-	}
-	std::string_view source{reinterpret_cast<const char *>(data.data()),
-	                        data.size()};
+// ============================================================================
+// Shaders
+// ============================================================================
 
-	GLuint shader = glCreateShader(shaderType);
-	if (shader == 0) {
-		FAIL("Could not create shader.");
-	}
+struct Shader {
+	GLuint shader;
+};
 
-	CHECK(source.size() <= std::numeric_limits<GLint>::max());
-	const char *srcText[1] = {source.data()};
-	const GLint srcLen[1] = {static_cast<GLint>(source.size())};
-	glShaderSource(shader, 1, srcText, srcLen);
-	glCompileShader(shader);
-	GLint status;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (!status) {
-		FAIL("Shader failed to compile.");
-	}
-	return shader;
+std::array<Shader, ShaderCount> Shaders;
+
+// Compile a shader, given the source code for that shader.
+void CompileShader(int shaderId, std::string_view source) {
+	Shader &shader = Shaders[shaderId];
+	const char *ptr[1] = {source.data()};
+	const int len[1] = {static_cast<int>(source.size())};
+	glShaderSource(shader.shader, 1, ptr, len);
+	glCompileShader(shader.shader);
 }
 
-GLuint LinkProgram(GLuint vertex, GLuint fragment) {
-	GLuint program = glCreateProgram();
-	if (program == 0) {
-		FAIL("Could not create shader program.");
-	}
-
-	glAttachShader(program, vertex);
-	glAttachShader(program, fragment);
-	glLinkProgram(program);
-	glDetachShader(program, vertex);
-	glDetachShader(program, fragment);
-	GLint status;
-	glGetProgramiv(program, GL_LINK_STATUS, &status);
-	if (!status) {
-		FAIL("Shader program failed to link.");
-	}
-	return program;
-}
-
-// Compile the shaders that have been embedded into the program.
+// Compile all shaders using the shader source code embedded in the exeuctable.
 void CompileEmbedded() {
-	std::array<ShaderSource, ShaderCount> source = GetEmbeddedShaderSource();
-	std::array<GLuint, ShaderCount> shaders;
-	for (int i = 0; i < ShaderCount; i++) {
-		GLuint shader = glCreateShader(
-			i < VertexShaderCount ? GL_VERTEX_SHADER : GL_FRAGMENT_SHADER);
-		if (shader == 0) {
-			FAIL("Could not create shader.");
-		}
-		shaders[i] = shader;
-		glShaderSource(shader, 1, &source[i].ptr, &source[i].size);
-		glCompileShader(shader);
+	std::array<ShaderSource, ShaderCount> sources = GetEmbeddedShaderSource();
+	for (int shaderId = 0; shaderId < ShaderCount; shaderId++) {
+		const ShaderSource source = sources[shaderId];
+		const std::string_view sourceText{
+			source.ptr, static_cast<std::size_t>(source.size)};
+		CompileShader(shaderId, sourceText);
 	}
-	std::array<GLuint, ProgramCount> programs;
-	for (int i = 0; i < ProgramCount; i++) {
-		GLuint program = glCreateProgram();
-		if (program == 0) {
-			FAIL("Could not create program.");
-		}
-		programs[i] = program;
-		const ProgramSpec &spec = ProgramSpecs[i];
-		glAttachShader(program, shaders[spec.vertex]);
-		glAttachShader(program, shaders[spec.fragment]);
-		glLinkProgram(program);
-
-		GLint status;
-		glGetProgramiv(program, GL_LINK_STATUS, &status);
-		if (!status) {
-			FAIL("Shader program failed to link.");
-		}
-
-		glDetachShader(program, shaders[spec.vertex]);
-		glDetachShader(program, shaders[spec.fragment]);
-	}
-	for (int i = 0; i < ShaderCount; i++) {
-		glDeleteShader(shaders[i]);
-	}
-	TriangleProgram = programs[0];
-	CubeProgram = programs[1];
-	MVP = glGetUniformLocation(CubeProgram, "MVP");
 }
+
+// TODO: Don't hardcode this.
+const std::string_view ShaderFilenames[ShaderCount] = {
+	"triangle.vert",
+	"cube.vert",
+	"triangle.frag",
+	"cube.frag",
+};
 
 // Compile shaders from the filesystem.
 void CompileFiles() {
-	GLuint vertex = CompileShader(GL_VERTEX_SHADER, "triangle.vert");
-	GLuint fragment = CompileShader(GL_FRAGMENT_SHADER, "triangle.frag");
-	GLuint program = LinkProgram(vertex, fragment);
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
-	TriangleProgram = program;
+	std::vector<unsigned char> data;
+	std::string filename;
+	for (int shaderId = 0; shaderId < ShaderCount; shaderId++) {
+		filename.assign("shader/");
+		filename.append(ShaderFilenames[shaderId]);
+		if (!ReadFile(&data, filename)) {
+			FAIL("Could not read shader.", log::Attr{"filename", filename});
+		}
+		std::string_view sourceText{reinterpret_cast<const char *>(data.data()),
+		                            data.size()};
+		CompileShader(shaderId, sourceText);
+	}
+}
 
-	vertex = CompileShader(GL_VERTEX_SHADER, "cube.vert");
-	fragment = CompileShader(GL_FRAGMENT_SHADER, "cube.frag");
-	program = LinkProgram(vertex, fragment);
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
-	CubeProgram = program;
+// ============================================================================
+// Shader Programs
+// ============================================================================
+
+struct Program {
+	GLuint program;
+};
+
+std::array<Program, ProgramCount> Programs;
+
+// Link all shader programs.
+void LinkPrograms() {
+	// Link and then check status separately. This way, the driver can compile
+	// shaders in parallel.
+	for (int programId = 0; programId < ProgramCount; programId++) {
+		Program &program = Programs[programId];
+		glLinkProgram(program.program);
+	}
+
+	for (int programId = 0; programId < ProgramCount; programId++) {
+		Program &program = Programs[programId];
+		GLint status;
+		glGetProgramiv(program.program, GL_LINK_STATUS, &status);
+		if (!status) {
+			FAIL("Shader program failed to link.");
+		}
+	}
+
+	TriangleProgram = Programs[0].program;
+	CubeProgram = Programs[1].program;
+	MVP = glGetUniformLocation(CubeProgram, "MVP");
 }
 
 } // namespace
+
+// ============================================================================
+// Initialization
+// ============================================================================
 
 GLuint TriangleProgram;
 GLuint CubeProgram;
 GLint MVP;
 
 void Init() {
-	os_string_view view = var::ProjectPath.get();
-	if (var::ProjectPath.get().empty()) {
-		CompileEmbedded();
-	} else {
-		CompileFiles();
+	// Create shader objects.
+	for (int shaderId = 0; shaderId < ShaderCount; shaderId++) {
+		GLuint shader =
+			glCreateShader(shaderId < VertexShaderCount ? GL_VERTEX_SHADER
+		                                                : GL_FRAGMENT_SHADER);
+		if (shader == 0) {
+			FAIL("Could not create shader.");
+		}
+		Shaders[shaderId].shader = shader;
 	}
 
-	MVP = glGetUniformLocation(CubeProgram, "MVP");
+	// Create shader program objects and attach shaders.
+	for (int programId = 0; programId < ProgramCount; programId++) {
+		GLuint program = glCreateProgram();
+		if (program == 0) {
+			FAIL("Could not create program.");
+		}
+		Programs[programId].program = program;
+		const ProgramSpec &spec = ProgramSpecs[programId];
+		glAttachShader(program, Shaders[spec.vertex].shader);
+		glAttachShader(program, Shaders[spec.fragment].shader);
+	}
+
+	// Figure out where shader source code is coming from.
+	os_string_view view = var::ProjectPath.get();
+	if (var::ProjectPath.get().empty()) {
+		// No ProjectPath, so we only have the embedded shaders code. Compile
+		// and link, and then destroy the shader objects since we do not need
+		// them any more.
+		CompileEmbedded();
+		LinkPrograms();
+		for (int programId = 0; programId < ProgramCount; programId++) {
+			const GLuint program = Programs[programId].program;
+			const ProgramSpec &spec = ProgramSpecs[programId];
+			glDetachShader(program, Shaders[spec.vertex].shader);
+			glDetachShader(program, Shaders[spec.fragment].shader);
+		}
+		for (int shaderId = 0; shaderId < ShaderCount; shaderId++) {
+			Shader &shader = Shaders[shaderId];
+			glDeleteShader(shader.shader);
+			shader.shader = 0;
+		}
+	} else {
+		CompileFiles();
+		LinkPrograms();
+	}
 }
 
 } // namespace gl_shader
