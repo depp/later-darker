53c0f7abd27246e8f7378573f455342ce2f53a21
Dietrich Epp <depp@zdome.net>
2025-03-17 03:10:57 -0400

    Parse shader spec file
    This spec file describes which shaders should be compiled and linked
    together.

--- tools/src/main.rs
+++ tools/src/main.rs
@@ -1,13 +1,28 @@
+use std::error::Error;
 use std::path::PathBuf;
+use std::process;
 
 use clap::Parser;
+
+mod spec;
 
 #[derive(Parser, Debug)]
 struct Args {
     spec: PathBuf,
 }
 
+fn run(args: &Args) -> Result<(), Box<dyn Error>> {
+    let progs = spec::Program::read(&args.spec)?;
+    for prog in progs.iter() {
+        eprintln!("Program: {:?}", prog);
+    }
+    Ok(())
+}
+
 fn main() {
     let args = Args::parse();
-    eprintln!("args = {:?}", args);
+    if let Err(e) = run(&args) {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
