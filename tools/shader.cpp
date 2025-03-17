91ad2aad51a7bc65b3737226882dacc66f77b5e0
Dietrich Epp <depp@zdome.net>
2025-03-17 01:17:30 -0400

    Add tools subdirectory
    This directory will contain the source code for tools to build the main
    demo.

---
+++ tools/shader.cpp
@@ -0,0 +1,11 @@
+// Copyright 2025 Dietrich Epp <depp@zdome.net>
+// Licensed under the Mozilla Public License Version 2.0.
+// SPDX-License-Identifier: MPL-2.0
+
+// Pack: 
+#include <cstdio>
+
+int main(int argc, char **argv) {
+	std::puts("Pack!");
+	return 0;
+}
