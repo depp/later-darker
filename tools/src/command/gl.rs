066f7b9fe3dd59ff18f0935c2c157b66d887ad6d
Dietrich Epp <depp@zdome.net>
2025-03-18 12:00:50 -0400

    Refactor: Extract find_cpp_files function

--- tools/src/command/gl.rs
+++ tools/src/command/gl.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
-use std::path::PathBuf;
+use std::io;
+use std::path::{Path, PathBuf};
 
 use clap::Parser;
 
@@ -15,25 +16,35 @@ impl Args {
     pub fn run(&self) -> Result<(), Box<dyn Error>> {
         let mut directory = self.srcdir.clone();
         directory.push("src");
-        for item in fs::read_dir(&directory)? {
-            let item = item?;
-            let mut file_path = directory.clone();
-            file_path.push(item.file_name());
-            let Some(ext) = file_path.extension() else {
-                continue;
-            };
-            let Some(ext) = ext.to_str() else {
-                continue;
-            };
-            if ext != "cpp" {
-                continue;
-            }
-            let text = fs::read_to_string(&file_path)?;
-            eprintln!("File: {}", file_path.file_name().unwrap().to_str().unwrap());
+        let files = find_cpp_files(&directory)?;
+        for file in files.iter() {
+            let text = fs::read_to_string(&file)?;
+            eprintln!("File: {}", file.file_name().unwrap().to_str().unwrap());
             for ident in identifier::Identifiers::new(&text) {
                 eprintln!("  {:?}", ident);
             }
         }
         Ok(())
     }
+}
+
+/// List all C++ implementation files in the given directory.
+fn find_cpp_files(directory: &Path) -> io::Result<Vec<PathBuf>> {
+    let mut files = Vec::new();
+    for item in fs::read_dir(&directory)? {
+        let item = item?;
+        let mut file_path = directory.to_path_buf();
+        file_path.push(item.file_name());
+        let Some(ext) = file_path.extension() else {
+            continue;
+        };
+        let Some(ext) = ext.to_str() else {
+            continue;
+        };
+        if ext != "cpp" {
+            continue;
+        }
+        files.push(file_path);
+    }
+    Ok(files)
 }
